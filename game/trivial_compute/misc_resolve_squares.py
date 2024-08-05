38b14732d79a05d418043d600e775b800ea86837
Steven Qian <99212270+qian-steven@users.noreply.github.com>
2024-08-05 16:33:42 +0000

    adding logic to resolve squares when landed on

---
+++ game/trivial_compute/misc_resolve_squares.py
@@ -0,0 +1,31 @@
+def resolve_square(Player):
+    event = get_square()
+    if "category" in event:
+        event_parts = event.split('|')
+        correct = run_question_gui_instance(event_parts[1])
+        if correct:
+            return "reroll"
+        else:
+            return "end"
+        
+    if "HQ" in event:
+        event_parts = event.split('|')
+        correct = run_question_gui_instance(event_parts[1])
+        if correct:
+            addScore(event_parts[1],Player)
+            return "reroll"
+        else:
+            return "end"
+    
+    if event == "Roll Again":
+        return "reroll"
+    
+    if event == "Trivial Compute":
+        if check_win_condition(Player) == False:
+            return "end"
+        category = run_category_selection_gui()
+        correct = run_question_gui_instance(category)
+        if correct:
+            return "win game"
+        else:
+            return "end"
